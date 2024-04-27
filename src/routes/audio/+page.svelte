e4d7f469be2810e61a76bb80ec9a1f4d232ee3f5
shaammiru <msyamil.hamami@gmail.com>
2024-04-27 23:16:39 +0700

    Setup tailwind and daisyui

---
+++ src/routes/audio/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head><title>Alterease | Audio</title></svelte:head>
+
+<div class="m-10">
+	<h1 class="font-bold text-xl text-center">Audio Compression</h1>
+</div>
---
+++ src/routes/image/+page.svelte
@@ -0,0 +1,5 @@
+<svelte:head><title>Alterease | Image</title></svelte:head>
+
+<div class="m-10">
+	<h1 class="font-bold text-xl text-center">Image Resizer</h1>
+</div>
