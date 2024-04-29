2859ae19e849ab7c872f78a504057e4b1f744dfa
shaammiru <msyamil.hamami@gmail.com>
2024-04-29 17:06:08 +0700

    Update image and audio pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
 <svelte:head><title>Alterease</title></svelte:head>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content text-center">
+		<div class="max-w-md">
+			<h1 class="text-5xl font-bold">Alterease</h1>
+			<p class="py-6">Simple web for resizing image and comressing audio.</p>
+			<div class="space-x-2">
+				<a href="/image"><button class="btn btn-primary">Resize Image</button></a>
+				<a href="/audio"><button class="btn btn-primary">Compress Audio</button></a>
+			</div>
+		</div>
+	</div>
+</div>
